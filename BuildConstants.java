b1653164454a12743bc7ebf4727c53b97482d76b
Avni Tewari <114118515+avnitewari@users.noreply.github.com>
2026-02-01 15:39:02 -0800

    2/1 limelight vision simple scan

---
+++ BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "skywalkers2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "276ae1a3bc3ae03dc9bdffbb144908629b2d4ad0";
+  public static final String GIT_DATE = "2026-01-19 17:45:20 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-21 00:26:55 EST";
+  public static final long BUILD_UNIX_TIME = 1768973215771L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
