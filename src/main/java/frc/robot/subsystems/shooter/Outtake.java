1d478daf084b56efa8700e785a2856570a794d2b
Ronit <ninja.ronit@gmail.com>
2026-03-09 19:56:05 -0700

    integrating motors for testing4

---
+++ src/main/java/frc/robot/subsystems/shooter/Outtake.java
@@ -0,0 +1,83 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.OuttakeConstants;
+import frc.robot.util.Limelight;
+import org.littletonrobotics.junction.Logger;
+
+public class Outtake extends SubsystemBase {
+
+  private final OuttakeIO io;
+  private final OuttakeIOInputsAutoLogged inputs = new OuttakeIOInputsAutoLogged();
+
+  public Outtake(OuttakeIO io) {
+    this.io = io;
+  }
+
+  private double calculateVoltage(double distance) {
+
+    // example tuning curve
+    double voltage;
+
+    if (Limelight.getDistanceMeters() < 2.1) { 
+      voltage = 5.4;
+      System.out.println(Limelight.getDistanceMeters());
+    } else if (Limelight.getDistanceMeters() < 2.5) {
+      voltage = 5.88;
+      System.out.println(Limelight.getDistanceMeters());
+    } else if (Limelight.getDistanceMeters() < 2.8) {
+      voltage = 6.3;
+      System.out.println(Limelight.getDistanceMeters());
+    } else {
+      voltage = 0;
+      System.out.println(Limelight.getDistanceMeters());
+    }
+
+    return voltage;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs(OuttakeConstants.kLoggingPath, inputs);
+  }
+
+  // presets
+  public void ampScore() {
+    io.setVoltage(5.8);
+  }
+
+  public void feed() {
+    io.setVelocityRPM(OuttakeConstants.kFeedRPM);
+  }
+
+  public void reverse() {
+    io.setVelocityRPM(OuttakeConstants.kReverseRPM);
+  }
+
+  public void idleHold() {
+    io.setVelocityRPM(OuttakeConstants.kIdleHoldRPM);
+  }
+
+  public void scoreWithVision() {
+
+    if (!Limelight.hasTarget()) {
+      stop();
+      return;
+    }
+
+    double distance = Limelight.getDistanceMeters();
+
+    double voltage = calculateVoltage(distance);
+
+    io.setVoltage(voltage);
+  }
+
+  public void runPercent(double percent) {
+    io.setVoltage(percent * 40.0); // 12.0, 12.5, 13.5, 16, 40
+  }
+
+  public void stop() {
+    io.stop();
+  }
+}
