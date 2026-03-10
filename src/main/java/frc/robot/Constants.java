62eca12a1f9e68b2c8e7f03170459bba1348503d
Ronit <ninja.ronit@gmail.com>
2026-03-09 20:04:47 -0700

    integrating motors for testing7

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,9 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -59,6 +62,59 @@ public final class Constants {
     public static final double JAM_REVERSE_VOLTAGE = -2.0;
     public static final double JAM_REVERSE_TIME = 0.2;
   }
+
+  public static final class OuttakeConstants {
+
+
+   // can
+   public static final int MOTOR_ID = 40; // change
+   public static final String kCanBus = "rio";
+
+
+   // motor settings
+   public static final InvertedValue MOTOR_INVERTED = InvertedValue.Clockwise_Positive;
+   public static final NeutralModeValue kNeutralMode = NeutralModeValue.Coast; // test value
+
+
+   // current limits
+   public static final double kStatorCurrentLimit = 100.0; // 60.0, 80
+   public static final double kSupplyCurrentLimit = 80.0; // 40.0, 43.0, 60
+
+
+   // velocity setpoints WILL TUNE FS
+   public static final double kAmpScoreRPM = 6000.0; // 5000.0, 6000.0, 8000,0, 10000
+   public static final double kFeedRPM = 2400.0; // 1200
+   public static final double kReverseRPM = -2000.0;
+   public static final double kIdleHoldRPM = 300.0;
+
+
+   // jam detection
+   public static final double kJamCurrentThreshold = 60.0; // 50.0
+
+
+   // gEAR-ratio:
+   public static final double kGearRatio = 0.25;
+
+
+   // Limelight settings
+   public static final String LIMELIGHT_NAME = "limelight";
+
+
+   // geometry (meters)
+   public static final double LIMELIGHT_HEIGHT = 0.4318; // height of camera from floor
+   public static final double TARGET_HEIGHT = 1.8288; // april tag height
+   public static final double LIMELIGHT_ANGLE_DEG = 10.0; // mounting angle
+
+
+   // voltage limits
+   public static final double kMinVoltage = 4.5;
+   public static final double kMaxVoltage = 8.0;
+
+
+   // logging pth
+   public static final String kLoggingPath = "outtake/";
+ }
+
 
   public static final class TransferConstants {
     // CAN IDs for the transfer motors
