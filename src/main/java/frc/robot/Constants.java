04241927fcc4032e602f5ce98d59d00ef850d654
Ronit <ninja.ronit@gmail.com>
2026-03-10 20:06:25 -0700

    Outtake + transfer tuning6

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,6 +31,43 @@ public final class Constants {
     REPLAY
   }
 
+  public static final class IntakeConstants {
+    // motor settings (PLS CHANGE B4 TESTING)
+    public static final int PIVOT_ID = 42;
+    public static final int ROLLER_ID = 55;
+    public static final int CANCODER_ID = 52;
+
+    // inversion settings (IDK IM JS GUESSING PLS TUNE)
+    public static final boolean PIVOT_INVERTED = false;
+    public static final boolean ROLLER_INVERTED = false;
+
+    // current lims
+    public static final double STATOR_LIMIT_AMPS = 40.0;
+    public static final double JAM_CURRENT_THRESHOLD = 35.0;
+
+    // pivot (js guessing, pls check w design or sumn to see what they want)
+    public static final double STOWED_DEG = 0.0;
+    public static final double HANDOFF_DEG = 35.0;
+    public static final double DEPLOYED_DEG = 75.0;
+
+    // jam detection nums
+    public static final double PIVOT_kP = 60.0;
+    public static final double PIVOT_kI = 0.0;
+    public static final double PIVOT_kD = 2.0;
+    public static final double PIVOT_kG = 0.3; // gravity feedforward
+    public static final double CRUISE_VELOCITY = 60.0; // rotations/sec
+    public static final double ACCELERATION = 120.0;
+
+    // roller volltages
+    public static final double INTAKE_VOLTAGE = 1.0;
+    public static final double OUTTAKE_VOLTAGE = -1.0;
+    public static final double HANDOFF_VOLTAGE = 4.0;
+
+    // more jam handling constants
+    public static final double JAM_REVERSE_VOLTAGE = -6.0;
+    public static final double JAM_REVERSE_TIME = 0.2; // seconds
+  }
+  
   public static final class OuttakeConstants {
 
     // can
